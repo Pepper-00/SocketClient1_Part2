85891cb563806cf92edaa4f8642227b8e2904d60
Pepper <eveeelyyyn0@gmail.com>
2024-08-07 16:13:28 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //establish tcp client 
+                TcpClient client = new TcpClient();
+                client.Connect("127.0.0.1", 62443);
+                Console.WriteLine("Client2 connected to server.");
+
+
+                //input string 
+                Console.WriteLine("Enter the message you want to send.");
+                string upload_string = Console.ReadLine();
+                Stream stream = client.GetStream();
+
+                //set up buffer 
+                UTF8Encoding encoding = new UTF8Encoding();
+                byte[] bytes = encoding.GetBytes(upload_string);
+
+                //write msg to stream
+                stream.Write(bytes, 0, bytes.Length);
+
+                byte[] bytes1 = new byte[1024];
+                int y = stream.Read(bytes1);
+                string record = Encoding.UTF8.GetString(bytes1);
+
+                //Console.WriteLine($"Received from server1: {record}");
+
+                //close connection 
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SocketClient1_Part2
+ [client side of SocketServer Project] under the same IP address, there are 2 server and their following clients. This project allows client1 talks to server 2 and vice versa while implementing gRPC. using c#, .net, socket, gRPC
